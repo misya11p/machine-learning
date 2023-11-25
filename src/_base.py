3b60be6bf1998358984cb35531e3cb19ec8a2132
misya11p <miya33ps@gmail.com>
2023-11-25 16:10:27 +0900

    rename

--- src/_base.py
+++ src/_base.py
@@ -1,31 +1,111 @@
+from abc import ABCMeta, abstractmethod
 import numpy as np
+from .metrics import accuracy_score, r2_score
 
 
-def accuracy_score(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-    """
-    Accuracy classification score.
+class BaseClassifire(metaclass=ABCMeta):
+    """Base class for all classifires"""
 
-    Args:
-        y_true (np.ndarray): 1d numpy array of correct class labels.
-        y_pred (np.ndarray): 1d numpy array of predicted class labels.
+    @abstractmethod
+    def fit(self, X, y):
+        pass
 
-    Returns:
-        float: Accuracy score.
-    """
-    return np.mean(y_true == y_pred)
+    @abstractmethod
+    def predict(self, X):
+        pass
+
+    def score(self, X: np.ndarray, y: np.ndarray) -> float:
+        """
+        Return accuracy score.
+
+        Args:
+            X (np.ndarray):
+                Test samples. 2d numpy array of shape
+                (n_samples, n_features).
+            y (np.ndarray):
+                True labels for `X`. 1d numpy array  of shape
+                (n_samples,).
+
+        Returns:
+            float: Mean accuracy of `self.predict(X)` w.r.t. `y`.
+        """
+        return accuracy_score(y, self.predict(X))
 
 
-def r2_score(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-    """
-    R2 (coefficient of determination) regression score function.
+class BaseRegressor(metaclass=ABCMeta):
+    """Base class for all regressors"""
 
-    Args:
-        y_true (np.ndarray): 1d numpy array of correct target values.
-        y_pred (np.ndarray): 1d numpy array of predicted target values.
+    @abstractmethod
+    def fit(self, X, y):
+        pass
 
-    Returns:
-        float: R2 score.
-    """
-    sse = np.sum((y_true - y_pred) ** 2)
-    sst = np.sum((y_true - np.mean(y_true)) ** 2)
-    return 1 - sse/sst
+    @abstractmethod
+    def predict(self, X):
+        pass
+
+    def score(self, X: np.ndarray, y: np.ndarray) -> float:
+        """
+        Return R^2 score.
+
+        Args:
+            X (np.ndarray):
+                Test samples. 2d numpy array of shape
+                (n_samples, n_features).
+            y (np.ndarray):
+                True values for `X`. 1d numpy array  of shape
+                (n_samples,).
+
+        Returns:
+            float: R^2 score of `self.predict(X)` w.r.t. `y`.
+        """
+        return r2_score(y, self.predict(X))
+
+
+class BaseCluster(metaclass=ABCMeta):
+    """Base class for all clusterers"""
+
+    @abstractmethod
+    def fit(self, X):
+        pass
+
+    @abstractmethod
+    def predict(self, X):
+        pass
+
+    def fit_predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Fit training data and predict labels.
+
+        Args:
+            X (np.ndarray): 2d numpy array of training data.
+
+        Returns:
+            np.ndarray: 1d numpy array of predicted labels.
+        """
+        self.fit(X)
+        return self.predict(X)
+
+
+class BaseTransformer(metaclass=ABCMeta):
+    """Base class for all transformers"""
+
+    @abstractmethod
+    def fit(self, X):
+        pass
+
+    @abstractmethod
+    def transform(self, X):
+        pass
+
+    def fit_transform(self, X: np.ndarray) -> np.ndarray:
+        """
+        Fit training data and transform it.
+
+        Args:
+            X (np.ndarray): 2d numpy array of training data.
+
+        Returns:
+            np.ndarray: 2d numpy array of transformed data.
+        """
+        self.fit(X)
+        return self.transform(X)
--- src/metrics.py
+++ src/metrics.py
@@ -1,111 +1,31 @@
-from abc import ABCMeta, abstractmethod
 import numpy as np
-from .metrics import accuracy_score, r2_score
 
 
-class BaseClassifire(metaclass=ABCMeta):
-    """Base class for all classifires"""
+def accuracy_score(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """
+    Accuracy classification score.
 
-    @abstractmethod
-    def fit(self, X, y):
-        pass
+    Args:
+        y_true (np.ndarray): 1d numpy array of correct class labels.
+        y_pred (np.ndarray): 1d numpy array of predicted class labels.
 
-    @abstractmethod
-    def predict(self, X):
-        pass
-
-    def score(self, X: np.ndarray, y: np.ndarray) -> float:
-        """
-        Return accuracy score.
-
-        Args:
-            X (np.ndarray):
-                Test samples. 2d numpy array of shape
-                (n_samples, n_features).
-            y (np.ndarray):
-                True labels for `X`. 1d numpy array  of shape
-                (n_samples,).
-
-        Returns:
-            float: Mean accuracy of `self.predict(X)` w.r.t. `y`.
-        """
-        return accuracy_score(y, self.predict(X))
+    Returns:
+        float: Accuracy score.
+    """
+    return np.mean(y_true == y_pred)
 
 
-class BaseRegressor(metaclass=ABCMeta):
-    """Base class for all regressors"""
+def r2_score(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """
+    R2 (coefficient of determination) regression score function.
 
-    @abstractmethod
-    def fit(self, X, y):
-        pass
+    Args:
+        y_true (np.ndarray): 1d numpy array of correct target values.
+        y_pred (np.ndarray): 1d numpy array of predicted target values.
 
-    @abstractmethod
-    def predict(self, X):
-        pass
-
-    def score(self, X: np.ndarray, y: np.ndarray) -> float:
-        """
-        Return R^2 score.
-
-        Args:
-            X (np.ndarray):
-                Test samples. 2d numpy array of shape
-                (n_samples, n_features).
-            y (np.ndarray):
-                True values for `X`. 1d numpy array  of shape
-                (n_samples,).
-
-        Returns:
-            float: R^2 score of `self.predict(X)` w.r.t. `y`.
-        """
-        return r2_score(y, self.predict(X))
-
-
-class BaseCluster(metaclass=ABCMeta):
-    """Base class for all clusterers"""
-
-    @abstractmethod
-    def fit(self, X):
-        pass
-
-    @abstractmethod
-    def predict(self, X):
-        pass
-
-    def fit_predict(self, X: np.ndarray) -> np.ndarray:
-        """
-        Fit training data and predict labels.
-
-        Args:
-            X (np.ndarray): 2d numpy array of training data.
-
-        Returns:
-            np.ndarray: 1d numpy array of predicted labels.
-        """
-        self.fit(X)
-        return self.predict(X)
-
-
-class BaseTransformer(metaclass=ABCMeta):
-    """Base class for all transformers"""
-
-    @abstractmethod
-    def fit(self, X):
-        pass
-
-    @abstractmethod
-    def transform(self, X):
-        pass
-
-    def fit_transform(self, X: np.ndarray) -> np.ndarray:
-        """
-        Fit training data and transform it.
-
-        Args:
-            X (np.ndarray): 2d numpy array of training data.
-
-        Returns:
-            np.ndarray: 2d numpy array of transformed data.
-        """
-        self.fit(X)
-        return self.transform(X)
+    Returns:
+        float: R2 score.
+    """
+    sse = np.sum((y_true - y_pred) ** 2)
+    sst = np.sum((y_true - np.mean(y_true)) ** 2)
+    return 1 - sse/sst
