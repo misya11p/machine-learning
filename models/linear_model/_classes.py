17a99114e1e52f07964ebeb8e116d49805955911
komiya <90894519+misya11p@users.noreply.github.com>
2024-04-08 02:23:53 +0900

    modify class

--- models/linear_model/_classes.py
+++ models/linear_model/_classes.py
@@ -2,20 +2,7 @@ import numpy as np
 from .._base import BaseClassifire, BaseRegressor, BaseCluster, BaseTransformer
 
 
-class LinearRegression(BaseRegressor):
-    def __init__(self):
-        self.weights = None
-
-    def fit(self, X, y):
-        X = np.insert(X, 0, 1, axis=1)
-        self.weights = np.linalg.inv(X.T @ X) @ X.T @ y
-
-    def predict(self, X):
-        X = np.insert(X, 0, 1, axis=1)
-        return X @ self.weights
-
-
-class Ridge(LinearRegression):
+class Ridge(BaseRegressor):
     def __init__(self, alpha: float = 1.0):
         super().__init__()
         self.alpha = alpha
@@ -25,6 +12,15 @@ class Ridge(LinearRegression):
         I = np.identity(X.shape[1], dtype=np.float32)
         I[0, 0] = 0
         self.weights = np.linalg.inv(X.T @ X + self.alpha*I) @ X.T @ y
+
+    def predict(self, X):
+        X = np.insert(X, 0, 1, axis=1)
+        return X @ self.weights
+
+
+class LinearRegression(Ridge):
+    def __init__(self):
+        super().__init__(alpha=0.0)
 
 
 class ElasticNet(LinearRegression):
@@ -50,12 +46,12 @@ class ElasticNet(LinearRegression):
         for _ in range(self.max_iter):
             self.weights[0] = np.mean(y - X[:, 1:] @ self.weights[1:])
             for i in range(1, n_features):
-                r = y - np.delete(X, i, 1) @ np.delete(self.weights, i)
+                r_i = y - np.delete(X, i, 1) @ np.delete(self.weights, i)
                 x_i = X[:, i]
                 norm = np.linalg.norm(x_i) ** 2
                 norm += n_samples * self.alpha * (1 - self.l1_ratio)
                 self.weights[i] = self.soft_thresholding(
-                    r @ x_i / norm,
+                    r_i @ x_i / norm,
                     n_samples * self.alpha * self.l1_ratio / norm
                 )
 
